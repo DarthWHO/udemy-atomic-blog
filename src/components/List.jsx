b4c148e99e80249b6886c2c6966ceed955e43077
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2025-12-09 22:17:41 -0700

    minor change to add test component for testing slow renders

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,20 @@
 import { usePosts } from "./PostProvider";
+import Test from "./Test";
 
 function List() {
   const { posts } = usePosts();
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {/* <Test /> */}
+    </>
   );
 }
 
